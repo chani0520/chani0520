6b379e4d8f1dd41bd80cbcb4cc48778da7db329c
chani0520 <kimchan0765@gmail.com>
2022-08-05 16:03:46 +0900

    studied nestjs

--- study_diary/nestjs/project/sample2/src/app.controller.ts
+++ study_diary/nestjs/project/sample2/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/hello')
+  getHello(@Req() req: Request): string {
+    /**
+     *  nest에서 이런식으로 요청객체를 다루는 경우는 드물다 => nest가 @Query(), @Param(key?: string), @Body 데코레이터를 제공하기 때문!
+     */
+    console.log(req);
     return this.appService.getHello();
   }
 }
